0925f0fc3ef2bfe46b34bd1f2dfa9da75a93fab5
user <kratika.bahgat.ets@gmail.com>
2021-05-13 18:30:02 +0530

    project done

--- wp-content/themes/skt-white-lite/discount-list.php
+++ wp-content/themes/skt-white-lite/discount-list.php
@@ -7,21 +7,34 @@ get_header();
 	global $post, $wpdb, $current_user;
 	// var_dump($current_user->ID);
 	// $discount_code = $_REQUEST['discount_code'];
-	$discount = $wpdb->get_row("SELECT code FROM $wpdb->pmpro_discount_codes");
-	// var_dump($discount);
+	$discounts = $wpdb->get_results("SELECT * FROM $wpdb->pmpro_discount_codes");
+	// $level = pmpro_getLevel(1);
+	$levels = $wpdb->get_results("SELECT * FROM $wpdb->pmpro_membership_levels");
+	 
+	 // var_dump($discounts);
+	 	
+	 
 ?>
 
 <div class="container"> 
 	<div class="row">
 		<div class="col-sm-6">
+	<?php 
+		foreach ($discounts as $discount) {
+ 	?>
 			<p><?php echo $discount->code; ?></p>
+		<?php } ?>
 		</div>
+
 		<div class="col-sm-6">
-			<p> Kratika </p>
+	<?php 
+		foreach ($levels as $level) {
+ 	?>
+			<p><?php echo $level->name; ?></p>
+		<?php } ?>
 		</div>
 
 	</div>
-
 </div>
 	
 	
